8fe1feed7387a9f65f25c5496ad74c963e76a7d8
Aryanpathak12002417 <aryanpathak989@gmail.com>
2023-08-06 22:11:20 +0530

    last commit

--- Server/Controllers/Comment.js
+++ Server/Controllers/Comment.js
@@ -1,5 +1,29 @@
 const router=require('express').Router()
+const db=require('../Models/Comment')
+const jwt=require('jsonwebtoken')
 
+router.post('/likeComment',(req,res)=>{
+    const {commentId}=req.body;
+    db.find({_id:commentId}).then(data=>{
+        if(req.user_id==data.user_id){
+            return res.json({"message":"You cannot like your Comment"})
+        }
+        db.findOneAndUpdate({_id:commentId},{like:data.like+1}).then(()=>{
+            res.json({"message":"Liked The Comment"})
+        }).catch(err=>{
+            res.json({"message":"cannot like the messgae"})
+        })
+
+    }).catch(err=>{
+        res.json({"message":"cannot like the message"})
+    })
+})
+
+
+
+router.post('/pagination',(req,res)=>{
+    
+})
 
 
 module.exports=router
